5c2b87768b7f34331e9dfd6ed2dcecefd07547ce
appkins <nbatkins@gmail.com>
2024-08-09 23:27:46 -0500

    refactor

--- modules/wpa-supplicant/example/main.tf
+++ modules/wpa-supplicant/example/main.tf
@@ -1,8 +1,17 @@
 module "udm_boot" {
   source = "../"
+
+  wan_interface = "eth10"
+  mac_address   = "88:96:4E:E3:A1:71"
+
+  ca_cert     = file("~/Downloads/EAP-TLS_8021x_001E46-R91NH8MB302415/CA_001E46-R91NH8MB302415.pem")
+  client_cert = file("~/Downloads/EAP-TLS_8021x_001E46-R91NH8MB302415/Client_001E46-R91NH8MB302415.pem")
+  private_key = file("~/Downloads/EAP-TLS_8021x_001E46-R91NH8MB302415/PrivateKey_PKCS1_001E46-R91NH8MB302415.pem")
+
   ssh = {
-    host     = "ui.appkins.io"
-    port     = 2222
-    username = "root"
+    host        = "ui.appkins.io"
+    port        = 2222
+    username    = "root"
+    private_key = file("~/.ssh/id_rsa")
   }
 }
