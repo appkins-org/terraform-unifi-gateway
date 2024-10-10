905259927cf5189b458521ecb593aa86084085b4
appkins <nbatkins@gmail.com>
2024-10-10 00:06:29 -0500

    Format

--- modules/shell/versions.tf
+++ modules/shell/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.4.2"
   required_providers {
-    http = { source = "hashicorp/http" }
+    http  = { source = "hashicorp/http" }
     shell = { source = "icj217/shell" }
   }
 }
